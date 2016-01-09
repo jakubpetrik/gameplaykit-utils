ad3a7f45e86a562027b4078148958a832aba0bf9
Jakub Petrík <petrik@inloop.eu>
2016-01-09 18:36:30 +0100

    Initial Commit

---
+++ GameplayKitUtils/GameplayKitUtils.h
@@ -0,0 +1,19 @@
+//
+//  GameplayKitUtils.h
+//  GameplayKitUtils
+//
+//  Created by Jakub Petrík on 1/9/16.
+//  Copyright © 2016 Jakub Petrik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GameplayKitUtils.
+FOUNDATION_EXPORT double GameplayKitUtilsVersionNumber;
+
+//! Project version string for GameplayKitUtils.
+FOUNDATION_EXPORT const unsigned char GameplayKitUtilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GameplayKitUtils/PublicHeader.h>
+
+
