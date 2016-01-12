e47d9fa8ee6088c439c48616bf521ba362d92c94
Jakub Petrík <petrik@inloop.eu>
2016-01-12 16:51:03 +0100

    Add generic Stack container

---
+++ GameplayKitUtils/Containers.swift
@@ -0,0 +1,29 @@
+//
+//  Containers.swift
+//  GameplayKitUtils
+//
+//  Created by Jakub Petrík on 1/12/16.
+//  Copyright © 2016 Jakub Petrik. All rights reserved.
+//
+
+import Foundation
+
+public struct Stack<T> {
+	private var items = [T]()
+
+	public var isEmpty: Bool {
+		return items.isEmpty
+	}
+
+	public mutating func push(item: T) {
+		items.append(item)
+	}
+
+	public mutating func pop() -> T {
+		return items.removeLast()
+	}
+
+	public func peek() -> T? {
+		return items.last
+	}
+}
